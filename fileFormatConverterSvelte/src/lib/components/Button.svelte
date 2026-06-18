c2e06487f3bb619031000c3892b498cc47b3afb9
StudentMan558 <s4168373@student.rmit.edu.au>
2026-06-18 15:20:48 +1000

    Resetup svelt

---
+++ fileFormatConverterSvelte/src/lib/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  interface Props {
+    label?: string;
+    onclick?: () => void;
+  }
+  
+  
+  let { label = 'Button', onclick }: Props = $props();
+</script>
+
+<button {onclick}>{label}</button>
+
+<style>
+button:hover {
+  background-color: black;
+}
+</style>
---
+++ fileFormatConverterSvelte/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+import Button from '$lib/components/Button.svelte';
+</script>
+
+<h1>File Format Converter</h1>
+<p>This website can convert different file types in other file types, currently supports:</p>
+
+<Button label="Rafael"/>
